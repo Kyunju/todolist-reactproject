49cb6f0b61430b26ba21d921e9b27a2dcb28d401
Kyungju <wwmix@naver.com>
2023-01-14 11:15:22 +0900

    로컬 스토리지에서 초기 상태 불러오기 및 상태 추가, 삭제

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TodoItems from './components/TodoItems';
 import TodoCounts from './components/TodoCounts';
 import TodoSubmit from './components/TodoSubmit';
 
 function App() {
-  const initialList = [
-    { id: 0, text: '드림코딩 매일 수강하기', checked: true },
-    { id: 1, text: '깃허브 매일 커밋하기', checked: false },
-    { id: 2, text: '꿈을 코딩하기', checked: false },
-  ];
+  const initialList = JSON.parse(localStorage.getItem('todoList'));
   const [items, setItem] = useState(initialList);
   const onRemove = (id) => {
     setItem(items.filter((item) => item.id !== id));
@@ -32,6 +28,11 @@ function App() {
       },
     ]);
   };
+
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(items));
+  }, [items]);
+
   return (
     <div className='wrapper'>
       <div className='contentBox'>
