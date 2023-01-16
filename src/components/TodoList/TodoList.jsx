834c16ea92fbe52e34fa3396a63205f0e6731b74
Kyungju <wwmix@naver.com>
2023-01-16 15:53:24 +0900

    리스트 구현

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+export default function TodoList() {
+  const [todos, setTodos] = useState() {
+    [
+      {id:'123', text: '장보기', status: 'active'},
+      {id:'124', text: '장보기', status: 'active'}
+    ]
+  }
+}
